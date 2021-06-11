61c0acd6d54b9d41d1fa8fd965b3952ce29e3093
Susmitha737 <lsusmitha94@gmail.com>
2021-06-11 16:21:56 +0530

    Committing test2 to branchx

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+        if (hungry == "yes")
+        	System.out.println("Eat");
+        else {
+        	  System.out.println("Do work");
+	         }
+ }
+}
